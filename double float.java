e27ac0dcff5a53ad3d856aa40de9aad3993b6eaf
arif670 <68595052+arif670@users.noreply.github.com>
2020-08-12 01:39:19 -0700

    Add files via upload

---
+++ double float.java
@@ -0,0 +1,15 @@
+class MyFirstJavaProg{
+public static void main(string vars[]){
+
+//float x=10.5;error
+float x=(float)10.5;
+float y=10.5f;
+
+
+double a=10.5;
+float b=(float)a;
+system.out.println(x);
+system.out.println(y);
+system.out.println(b);
+}
+}p
---
+++ type cast.java
@@ -0,0 +1,12 @@
+class MyFirstJavaProg{
+public static void main(string vars[]){
+char X='A';//2 byte
+int a=X;//4byte
+//implicit cast
+int y=65;
+//char z=y;error
+char z=(char)y;
+system.out.println(a);
+system.out.println(z);
+}
+}
